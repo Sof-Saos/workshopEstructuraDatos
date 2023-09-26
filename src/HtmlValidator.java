b223fde59672cb556e2809ec8768545aa3c10c32
Sof <74073693+Sof-Saos@users.noreply.github.com>
2023-09-26 17:20:00 -0500

    Add files via upload

--- src/HtmlValidator.java
+++ src/HtmlValidator.java
@@ -3,36 +3,37 @@ import java.util.Stack;
 
 public class HtmlValidator {
 	public static Stack<HtmlTag> isValidHtml(Queue<HtmlTag> tags) {
-		// Creamos una pila para rastrear las etiquetas abiertas.
 		Stack<HtmlTag> pila = new Stack<>();
+		// Iteramos mientras haya elementos en la cola
+		while (!tags.isEmpty()) {
+			HtmlTag etiquetaHtml = tags.poll(); // Obtenemos el siguiente elemento de la cola
 
-		for (HtmlTag etiquetaHtml : tags) {
-			// Si la etiqueta es de auto-cierre, la ignoramos y continuamos con la siguiente etiqueta.
 			if (etiquetaHtml.isSelfClosing()) {
+				// Si es una etiqueta de auto-cierre, no hacemos nada y continuamos
 				continue;
-			}
-			// Si es una etiqueta de apertura, la agregamos a la pila.
-			else if (etiquetaHtml.isOpenTag()) {
+			} else if (etiquetaHtml.isOpenTag()) {
+				// Si es una etiqueta de apertura, la agregamos a la pila
 				pila.push(etiquetaHtml);
-			}
-			// Si es una etiqueta de cierre, la comparamos con la etiqueta en la cima de la pila.
-			else if (!etiquetaHtml.isOpenTag()) {
+			} else if (!etiquetaHtml.isOpenTag()) {
+				// Si es una etiqueta de cierre, comparamos con la etiqueta en la cima de la pila
 				if (!pila.isEmpty()) {
-					// Comparamos la etiqueta de cierre con la etiqueta en la cima de la pila.
 					if (pila.peek().matches(etiquetaHtml)) {
-						// Si coinciden, eliminamos la etiqueta de la cima de la pila.
+						// Si coinciden, eliminamos la etiqueta de la cima de la pila
 						pila.pop();
 					} else {
-						// Si no coinciden, significa que las etiquetas no están equilibradas, por lo que retornamos la pila en su estado actual.
+						// Si no coinciden, retornamos la pila actual
 						return pila;
 					}
 				} else {
-					// Si la pila está vacía y encontramos una etiqueta de cierre, significa que no hay etiqueta de apertura correspondiente, por lo que retornamos null para indicar un error.
+					// Si la pila está vacía y encontramos una etiqueta de cierre, retornamos null
 					return null;
 				}
 			}
 		}
-		// Al final del bucle, si la pila está vacía, todas las etiquetas se han cerrado correctamente y retornamos una pila vacía para indicar que el HTML es válido.
+
+		// Al final del bucle, si la pila está vacía, todas las etiquetas se han cerrado correctamente
+		// y retornamos una pila vacía para indicar que el HTML es válido.
 		return pila;
 	}
 }
+
